39c75bbd08c0dc00eb3aede1bb50ca7c72df4825
Krushna Biradar <gmi.gj.dev.balajibk@gmail.com>
2024-09-30 16:24:41 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: '/api/auth/:path*',
+      }
+    ]
+  },
   images: {
     unoptimized: true,
     domains: [
